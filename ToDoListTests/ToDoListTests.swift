5b01048bdf1030b345f3866ca7cda2756da16c71
Varya Utkina <varya2412@mail.ru>
2024-11-21 09:48:34 +0300

    Create ToDoListTests

---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Варвара Уткина on 21.11.2024.
+//
+
+import XCTest
+@testable import ToDoList
+
+final class ToDoListTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        
+        
+        super.tearDown()
+    }
+    
+    func testExample() throws {
+        
+    }
+    
+    func testPerformanceExample() throws {
+        measure {
+            
+        }
+    }
+}
